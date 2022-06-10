04ea309ee99325f9dbb11f11e92ce0776e5f4699
ekofebriharsono <ekoaewes2006@gmail.com>
2022-06-10 21:20:09 +0700

    add introduce

--- mas_abap.py
+++ mas_abap.py
@@ -5,16 +5,25 @@ import text as t
 # inisialisasi Token Bot Kita
 bot = telebot.TeleBot('5538304225:AAGOS3Fibg_9Ji_seajAfcWPkBzT8Wog6Hw')
 
+modeHTML = 'HTML' 
+modeMarkdown = 'Markdown' 
+
 # Menghandle Pesan /start
 @bot.message_handler(commands=['start'])
 def welcome(message):
-    # membalas pesan
-    bot.reply_to(message, t.start)
+    bot.send_message(message.chat.id, t.start, parse_mode=modeMarkdown)
 
 @bot.message_handler(commands=['abap'])
 def welcome(message):
-    # membalas pesan
-    bot.reply_to(message, t.whatIsABAP)
+    bot.send_message(message.chat.id, t.whatIsABAP, reply_to_message_id=message.message_id, parse_mode=modeMarkdown)
+
+@bot.message_handler(commands=['alv'])
+def welcome(message):
+    bot.send_message(message.chat.id, t.whatIsALV, reply_to_message_id=message.message_id, parse_mode=modeMarkdown)
+
+@bot.message_handler(commands=['bapi'])
+def welcome(message):
+    bot.send_message(message.chat.id, t.whatIsBAPI, reply_to_message_id=message.message_id, parse_mode=modeMarkdown)
 
 while True:
     try:
--- text.py
+++ text.py
@@ -1,13 +1,14 @@
 welcome = "Hello welcome to Mas ABAP\n\n"
 control = "You can control me by sending these commands:\n\n"
 
-introduce = "<b>Introduce : </b>\n"
+introduce = "*Introduce : *\n"
 # Command Introduce
-i1 = "/abap \n"
-i2 = "/alv \n"
-i3 = "/rfc \n"
-i4 = "/bapi \n"
+i1 = "/abap - Programming Language SAP\n"
+i2 = "/alv - Report\n"
+i3 = "/bapi - Functional Module\n"
 
-start = welcome + control + introduce + i1 + i2 + i3 + i4
+start = welcome + control + introduce + i1 + i2 + i3
 
-whatIsABAP = "ABAP (Advanced Business Application Programming), is a fourth-generation programming language, used for development and customization purposes in the SAP software. Currently positioned along with Java, as the main language for SAP application server programming, most of the programs are executed under the control of the run-time system. This tutorial explains the key concepts of SAP ABAP."
+whatIsABAP = "*ABAP* (Advanced Business Application Programming), is a fourth-generation programming language, used for development and customization purposes in the SAP software. Currently positioned along with Java, as the main language for SAP application server programming, most of the programs are executed under the control of the run-time system. This tutorial explains the key concepts of SAP ABAP."
+whatIsALV = "*ALV* stands for *ABAP* List Viewer. ALV gives us a standard List format and user interface to all our ABAP reports. ALV is created by a set of standard function modules provided by SAP."
+whatIsBAPI = "*BAPI* (Business Application Programming Interface) is a standard interface to the business object models in SAP products. BAPIs are the primary method through which customer code and third-party applications interact with SAP products."
