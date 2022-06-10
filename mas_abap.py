7e3c6f8520137fbbb57f79162ca55f865805dc13
ekofebriharsono <ekoaewes2006@gmail.com>
2022-06-11 01:06:29 +0700

    add button create alv & contact us

--- mas_abap.py
+++ mas_abap.py
@@ -1,4 +1,5 @@
 # mengimport package pyTelegramBotAPI
+from cgitb import handler, text
 import telebot
 import text as t
 
@@ -9,21 +10,51 @@ modeHTML = 'HTML'
 modeMarkdown = 'Markdown' 
 
 # Menghandle Pesan /start
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['start', 'help'])
 def welcome(message):
     bot.send_message(message.chat.id, t.start, parse_mode=modeMarkdown)
 
 @bot.message_handler(commands=['abap'])
-def welcome(message):
+def abao(message):
     bot.send_message(message.chat.id, t.whatIsABAP, reply_to_message_id=message.message_id, parse_mode=modeMarkdown)
 
 @bot.message_handler(commands=['alv'])
-def welcome(message):
+def alv(message):
     bot.send_message(message.chat.id, t.whatIsALV, reply_to_message_id=message.message_id, parse_mode=modeMarkdown)
 
 @bot.message_handler(commands=['bapi'])
-def welcome(message):
+def bapi(message):
     bot.send_message(message.chat.id, t.whatIsBAPI, reply_to_message_id=message.message_id, parse_mode=modeMarkdown)
+
+@bot.message_handler(commands=['contactus'])
+def contactUs(message):
+    markup = telebot.types.InlineKeyboardMarkup()
+    button1 = telebot.types.InlineKeyboardButton("WhatsApp", url="wa.me/6289607244544")
+    button2 = telebot.types.InlineKeyboardButton("Telegram", url='t.me/ekofebriharsono')
+    markup.add(button1, button2)
+    bot.send_message(message.chat.id, "if there is a problem please contact the developer", reply_to_message_id=message.message_id, reply_markup=markup, parse_mode=modeMarkdown)
+
+@bot.message_handler(commands=['alvcreate'])
+def contactUs(message):
+    markup = telebot.types.InlineKeyboardMarkup()
+    button1 = telebot.types.InlineKeyboardButton("Fieldcat", callback_data="fieldcat")
+    button2 = telebot.types.InlineKeyboardButton("Layout", callback_data="layout")
+    markup.add(button1, button2)
+    msg = bot.send_message(message.chat.id, "Create ALV", reply_to_message_id=message.message_id, reply_markup=markup, parse_mode=modeMarkdown)
+    bot.register_callback_query_handler(msg, call_handler_create_alv)
+
+@bot.callback_query_handler(func=lambda call:True)
+def call_handler_create_alv(call):
+    if call.data == "fieldcat":
+        build_fieldcat(call)
+    elif call.data == "layout":
+        build_layout(call)
+
+def build_fieldcat(call):
+    bot.send_message(call.message.chat.id, "Build Fieldcat", parse_mode=modeMarkdown)
+
+def build_layout(call):
+    bot.send_message(call.message.chat.id, "Build Layout", parse_mode=modeMarkdown)
 
 while True:
     try:
--- text.py
+++ text.py
@@ -1,13 +1,23 @@
 welcome = "Hello welcome to Mas ABAP\n\n"
 control = "You can control me by sending these commands:\n\n"
 
-introduce = "*Introduce : *\n"
+
 # Command Introduce
+introduce = "*Introduce : *\n"
 i1 = "/abap - Programming Language SAP\n"
 i2 = "/alv - Report\n"
-i3 = "/bapi - Functional Module\n"
+i3 = "/bapi - Functional Module\n\n"
 
-start = welcome + control + introduce + i1 + i2 + i3
+
+# Command Create
+create = "*Create :* \n"
+c1 = "/alvcreate - Create ALV Report\n\n"
+
+# Command contact
+contact = "*Contact : *\n"
+h1 = "/contactus"
+
+start = welcome + control + introduce + i1 + i2 + i3 + create + c1 + contact + h1
 
 whatIsABAP = "*ABAP* (Advanced Business Application Programming), is a fourth-generation programming language, used for development and customization purposes in the SAP software. Currently positioned along with Java, as the main language for SAP application server programming, most of the programs are executed under the control of the run-time system. This tutorial explains the key concepts of SAP ABAP."
 whatIsALV = "*ALV* stands for *ABAP* List Viewer. ALV gives us a standard List format and user interface to all our ABAP reports. ALV is created by a set of standard function modules provided by SAP."
