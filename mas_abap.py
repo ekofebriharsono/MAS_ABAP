6ea1f851c9589b1bdd6491a229a253b9dafb1120
ekofebriharsono <ekoaewes2006@gmail.com>
2022-06-10 20:41:13 +0700

    Add simple command

--- mas_abap.py
+++ mas_abap.py
@@ -1,1 +1,23 @@
-print(123)
+# mengimport package pyTelegramBotAPI
+import telebot
+import text as t
+
+# inisialisasi Token Bot Kita
+bot = telebot.TeleBot('5538304225:AAGOS3Fibg_9Ji_seajAfcWPkBzT8Wog6Hw')
+
+# Menghandle Pesan /start
+@bot.message_handler(commands=['start'])
+def welcome(message):
+    # membalas pesan
+    bot.reply_to(message, t.start)
+
+@bot.message_handler(commands=['abap'])
+def welcome(message):
+    # membalas pesan
+    bot.reply_to(message, t.whatIsABAP)
+
+while True:
+    try:
+        bot.polling()
+    except:
+        pass
---
+++ text.py
@@ -0,0 +1,13 @@
+welcome = "Hello welcome to Mas ABAP\n\n"
+control = "You can control me by sending these commands:\n\n"
+
+introduce = "<b>Introduce : </b>\n"
+# Command Introduce
+i1 = "/abap \n"
+i2 = "/alv \n"
+i3 = "/rfc \n"
+i4 = "/bapi \n"
+
+start = welcome + control + introduce + i1 + i2 + i3 + i4
+
+whatIsABAP = "ABAP (Advanced Business Application Programming), is a fourth-generation programming language, used for development and customization purposes in the SAP software. Currently positioned along with Java, as the main language for SAP application server programming, most of the programs are executed under the control of the run-time system. This tutorial explains the key concepts of SAP ABAP."
